d03ee8a864d9eb39f53fca9344b75bf350260b06
romagolchin <gol4in.roman@yandex.ru>
2016-02-20 13:18:57 +0300

    Added files via upload

--- task1/main.c
+++ task1/main.c
@@ -3,32 +3,34 @@
 
 int main(int argc, const char* argr[]) {
 	int n, left, right, top, bottom;
-	scanf("%d", &n);
-	int** t = (int**) malloc(sizeof(int*) * n);
-	int** cur = t;
-	int it = 0;
-	if(!cur)
-		return -1;
-	while(it < n) {
-		int* cur1 = *cur = (int*) malloc(sizeof(int) * n);
-		if(!cur1)
-			return -1;
-		++cur;
-		++it;
+	for(;;) {
+        scanf("%d", &n);
+        int** t = (int**) malloc(sizeof(int*) * n);
+        int** cur = t;
+        int it = 0;
+        if(!cur)
+            return -1;
+        while(it < n) {
+            int* cur1 = *cur = (int*) malloc(sizeof(int) * n);
+            if(!cur1)
+                return -1;
+            ++cur;
+            ++it;
+        }
+
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j)
+                t[i][j] = (i + 1) * (j + 1);
+        }
+        scanf("%d", &left);
+        if(left) {
+            scanf("%d%d%d", &top, &right, &bottom);
+            for(int j = top - 1; j < bottom; ++j) {
+                for(int i = left - 1; i < right; ++i)
+                    printf("%d ", t[j][i]);
+                printf("\n");
+            }
+        }
 	}
-	
-	for(int i = 0; i < n; ++i) {
-		for(int j = 0; j < n; ++j)
-			t[i][j] = (i + 1) * (j + 1);
-	}
-	scanf("%d", &left);
-	if(left) {
-		scanf("%d%d%d", &top, &right, &bottom);
-		for(int j = top - 1; j < bottom; ++j) {
-			for(int i = left - 1; i < right; ++i)
-				printf("%d ", t[j][i]);
-			printf("\n");
-		}
-	}                
 	return 0;
 }
