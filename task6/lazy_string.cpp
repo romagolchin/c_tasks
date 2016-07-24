19097303c63a970c8024ff40450e2dcbce4923c5
romagolchin <gol4in.roman@yandex.ru>
2016-07-24 11:56:24 +0300

    Update lazy_string.cpp

--- task6/lazy_string.cpp
+++ task6/lazy_string.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 lazy_char::lazy_char(lazy_string* hs, size_t pos) :
-host_str(hs), pos(pos)
+	host_str(hs), pos(pos)
 {}
 
+//?!!!
 lazy_char::~lazy_char() {
 
 }
@@ -14,17 +15,15 @@ lazy_char::operator char() {
 }
 
 lazy_char& lazy_char::operator=(char c) {
-	//printf("%s %c\n", __func__, c);
-	if(host_str->has_reference) {
+	if (host_str->has_reference) {
 		char* s_changed = new char[host_str->len + 1];
-		for(size_t i = 0; i < host_str->len; ++i)
+		for (size_t i = 0; i < host_str->len; ++i)
 			s_changed[i] = host_str->s[i];
 		host_str->s = s_changed;
-		host_str->has_reference = 0;	
+		host_str->has_reference = 0;
 	}
 	*(host_str->s + pos) = c;
 	host_str->s[host_str->len] = '\0';
-	//
 	return *this;
 }
 
@@ -34,33 +33,21 @@ std::ostream& operator<<(std::ostream& stream, lazy_char lc) {
 }
 
 lazy_string::lazy_string(std::string str) {
-	//cerr << __func__ << endl;
-	//cerr << __func__ << endl;
-	cerr << "from std::string" << endl;
-	//fflush(stdout);
-
 	len = str.length();
 	s = new char[len + 1];
 	assert(s);
-	for(size_t i = 0; i < len; ++i)
+	for (size_t i = 0; i < len; ++i)
 		s[i] = str[i];
 	s[len] = '\0';
 	has_reference = 0;
 }
 
 lazy_string::lazy_string(char *s, size_t len, bool has_reference):
-s(s), len(len), has_reference(has_reference)
-{//cerr << __func__ << endl;
-//cerr << has_reference << endl;
-//this->out();
-}
+	s(s), len(len), has_reference(has_reference)
+{	}
 
 lazy_string::lazy_string(const lazy_string& source) {
 	using namespace std;
-	// #ifdef DEBUG 
-	cerr << "copy constructor" << endl;
-	//fflush(stdout);
-	// #endif
 	s = source.s;
 	len = source.len;
 	has_reference = 1;
@@ -68,35 +55,13 @@ lazy_string::lazy_string(const lazy_string& source) {
 
 lazy_string& lazy_string::operator=(lazy_string& source) {
 	using namespace std;
-		cerr << __func__ << endl;
-
-	#ifdef DEBUG 
-	//fflush(stdout);
-
-	this->out();
-	//fflush(stdout);
-	source.out();
-	//fflush(stdout);
-
-	#endif
-	// #ifdef DEBUG 
-	// if(!s)
-	// 	//printf("null\n");
-	// else
-	// 	//printf("%p\n", s);
-	// #endif
-	// delete [] s;
-	if(s) {
+	if (s) {
 		delete [] s;
 	}
 	s = source.s;
 	len = source.len;
 	has_reference = 1;
 	this->out();
-	//fflush(stdout);
-	//printf("%s\n", s);
-	//fflush(stdout);
-
 	return *this;
 }
 
@@ -109,7 +74,7 @@ lazy_char lazy_string::operator[](size_t pos) {
 }
 
 lazy_char lazy_string::at(size_t pos) {
-	if(pos >= len)
+	if (pos >= len)
 		throw out_of_range(std::to_string(pos) + " is out of range\n");
 	return (*this)[pos];
 }
@@ -120,9 +85,7 @@ lazy_string::operator std::string() {
 }
 
 lazy_string lazy_string::substr(size_t pos, size_t len) {
-	//printf("%p %d %d %p\n", s, pos, len, this);
-	cerr << __func__ << endl;
-	if(len > this->len)
+	if (len > this->len)
 		throw length_error("failed to take substring longer than string itself\n");
 	lazy_string ret(s + pos, len, 1);
 	return ret;
@@ -133,7 +96,6 @@ istream& operator>>(istream& stream, lazy_string& ls) {
 	string str;
 	stream >> str;
 	lazy_string tmp(str);
-	// ls = lazy_string(str);
 	ls = tmp;
 	return stream;
 }
@@ -144,14 +106,7 @@ ostream& operator<< (ostream& stream, lazy_string& ls) {
 }
 
 lazy_string::~lazy_string() {
-	#ifdef DEBUG 
-	//cerr << __func__ << endl;
-	//fflush(stdout);
-	//printf("%p\n", s);
-	//fflush(stdout);
-
-	#endif
-	if(!has_reference){
+	if (!has_reference) {
 		delete [] s;
 	}
 }
